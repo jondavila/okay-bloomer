eaa07b1c4418c128f62c9357d344e4dcb9132b75
Sydney Pogue <sp1441@Sydneys-Air.lan>
2023-07-15 16:23:20 -0400

    added imports, particularly for calendar/calendar styling

---
+++ src/app/components/AtGlance.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // import calendar css for styling
+
