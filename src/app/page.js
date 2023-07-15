ab834ed706177d8aa59571d35bac0bf123c1872c
Jonathan Davila <jonathandavila@Jonathans-MacBook-Air.local>
2023-07-15 15:09:54 -0700

    add: add alt text to profile image

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image'
-import SearchBar from './components/SearchBox'
+import SearchBar from './components/SearchBar'
 import Header from './components/Header'
 
 
