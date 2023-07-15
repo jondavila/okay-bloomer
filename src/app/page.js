ec3f753a7536a2cd155af9a7791b2d882e98186c
Jonathan Davila <jonathandavila@Jonathans-MacBook-Air.local>
2023-07-15 15:09:27 -0700

    feat: add SearchBar component

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
+'use client';
+
 import Image from 'next/image'
-import SearchBox from './components/SearchBox'
+import SearchBar from './components/SearchBox'
 import Header from './components/Header'
-import PlantCardGrid from './components/PlantCardGrid'
+
 
 export default function Home() {
   return (
     <>
       <Header pageTitle="Plant Search" profileImg="/path_to_profile_image.jpg" />
-      <SearchBox />
+      <SearchBar />
     </>
   )
 }
