5e6dec3579d1b0b647e342391eee10e5d5cb641f
badoodee95 <monzoncaleb@yahoo.com>
2023-07-16 05:09:58 -0700

    feat: added plantFaq component with a little tiny bit of styling

---
+++ src/app/components/PlantFaq.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function PlantFaq() {
+    return (
+        <div className="box has-text-centered has-text-weight-bold is-family-monospace has-background-info is-size-5 has-shadow" id="plantFaq" style={{ height: '300px', width: '300px', overflowY: 'auto' }}>
+            {/* Place your scrollable content here */}
+            <p>Question: Why are we here just to suffer? Is this reality or am I dreaming??</p>
+            <p>Answer: I dont fucking know, Im too goddamn old for this shit</p>
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
 'use client';
 
-import Image from 'next/image'
-import SearchBar from './components/SearchBar'
-import Header from './components/Header'
+import Image from 'next/image';
+import SearchBar from './components/SearchBar';
+import Header from './components/Header';
+import PlantFaq from './components/PlantFaq';
 
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
   return (
     <>
-      <Header pageTitle="Plant Search" profileImg="/path_to_profile_image.jpg" />
-      <SearchBar />
+      <Header pageTitle="Plant Sanctuary" profileImg="/path_to_profile_image.jpg" />
+      <PlantFaq />
     </>
-  )
+  );
 }
