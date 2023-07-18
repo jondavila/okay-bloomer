21dea03fb38907740fa497a583e80c9e60e75ec1
badoodee95 <monzoncaleb@yahoo.com>
2023-07-17 22:06:57 -0700

    feat: created plant type buttons/modals

---
+++ src/app/components/PlantTypeGrid.js
@@ -0,0 +1,19 @@
+import PlantType from './PlantType';
+import 'bulma/css/bulma.min.css';
+
+export default function PlantTypeGrid({ plantTypesArray }) {
+    let rows = [];
+    for (let i = 0; i < plantTypesArray.length; i++) {
+        let type = plantTypesArray[i];
+        rows.push(<PlantType key={i.toString()} type={type} />);
+    }
+    return (
+        <div>
+            <section className="container">
+                <div className="columns features is-multiline">
+                    {rows}
+                </div>
+            </section>
+        </div>
+    );
+}
