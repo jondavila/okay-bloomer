29662c6e2a8118eb540570d73ceb00b36c9e9535
Jonathan Davila <jonathandavila@Jonathans-MacBook-Air.local>
2023-07-18 20:42:12 -0700

    feat: add search result page

--- src/app/components/PlantCard.js
+++ src/app/components/PlantCard.js
@@ -2,25 +2,26 @@ import React from 'react';
 import Image from 'next/image';
 import 'bulma/css/bulma.min.css';
 
-export default function PlantCard() {
+export default function PlantCard(plant) {
     return (
         <div class="column is-3">
             <div class="card is-shady">
                 <div class="card-image">
                     <figure class="image is-4by3">
-                        <img src="https://images.unsplash.com/photo-1515555230216-82228b88ea98?auto=format&amp;fit=crop&amp;w=900&amp;q=60" alt="Placeholder image" class="modal-button" data-target="modal-image2" />
+                        <img src={plant.image} alt={`${plant.commonName} Image`} />
                     </figure>
                 </div>
                 <div class="card-content">
                     <div class="content">
-                        <h4>Click on image above</h4>
-                        <p>less words</p>
-                        <span class="button is-link modal-button" data-target="modal-image2">Image modal</span>
+                        <h4 className='is-size-4 has-text-weight-bold'>{plant.nickname}</h4>
+                        <h4 className='is-size-4 has-text-weight-bold'>{plant.commonName}</h4>
+                        <p className='is-size-6 is-italic'>{plant.scientificName}</p>
                     </div>
                 </div>
             </div>
         </div>
     );
 };
+
 
 
--- src/app/components/PlantCardGrid.js
+++ src/app/components/PlantCardGrid.js
@@ -1,36 +1,19 @@
-import PlantCard from "./PlantCard"
+import PlantType from './PlantType';
 import 'bulma/css/bulma.min.css';
 
-export default function PlantCardGrid({ }) {
+export default function PlantTypeGrid({ plantTypesArray }) {
+    let rows = [];
+    for (let i = 0; i < plantTypesArray.length; i++) {
+        let type = plantTypesArray[i];
+        rows.push(<PlantType key={i.toString()} type={type} />);
+    }
     return (
         <div>
             <section className="container">
                 <div className="columns features is-multiline">
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-                    <PlantCard />
-
-
+                    {rows}
                 </div>
             </section>
         </div>
-    )
+    );
 }
