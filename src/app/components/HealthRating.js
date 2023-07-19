b52103d690f04a3b94c09ba075176c4570274202
Sydney Pogue <sp1441@Sydneys-Air.lan>
2023-07-19 02:47:32 -0400

    edited import of css to health rating component, converted css to camelCase

--- src/app/components/HealthRating.js
+++ src/app/components/HealthRating.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './HealthRating.css';
+import styles from '../healthrating.module.css';
 
 export default function HealthRating({ tasks }) {
     const completedTasksCount = tasks.filter(task => task.status === 'completed').length;
@@ -8,13 +8,13 @@ export default function HealthRating({ tasks }) {
     return (
         <div>
             <h3>Health Rating</h3>
-            <svg viewBox="0 0 36 36" className="circular-chart">
-                <path className="circle-bg"
+            <svg viewBox="0 0 36 36" className={styles.circularChart}>
+                <path className={styles.circleBg}
                     d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" />
-                <path className="circle"
+                <path className={styles.circle}
                     stroke-dasharray={`${healthScore}, 100`}
                     d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" />
-                <text x="18" y="20.35" className="percentage">{healthScore}%</text>
+                <text x="18" y="20.35" className={styles.percentage}>{healthScore}%</text>
             </svg>
         </div>
     );
