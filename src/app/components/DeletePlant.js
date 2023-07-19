4957e39e8c951462cc59af8845e1bb68df945b17
Sydney Pogue <sp1441@Sydneys-Air.lan>
2023-07-19 07:58:46 -0400

    created delete plant component for delete plant button on individual-user-plant page

---
+++ src/app/components/DeletePlant.js
@@ -0,0 +1,35 @@
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function DeletePlant({ plantId, onDelete }) {
+    const [showConfirm, setShowConfirm] = useState(false);
+
+    const handleDelete = () => {
+        axios.delete(`http://localhost:4000/plants/${plantId}`)
+            .then(response => {
+                if (response.status === 200) {
+                    onDelete();
+                }
+            })
+            .catch(error => {
+                console.error('Error deleting plant: ', error);
+            });
+    };
+
+    if (showConfirm) {
+        return (
+            <div>
+                <h4>Are you sure you want to delete this plant? Once you do, it's gone forever.</h4>
+                <button onClick={handleDelete}>Delete</button>
+                <button onClick={() => setShowConfirm(false)}>Cancel</button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <button onClick={() => setShowConfirm(true)}>Remove Plant</button>
+        </div>
+    );
+}
