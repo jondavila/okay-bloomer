55d7f82015770c5f1a41cc0bc21b227c5c3455df
Sydney Pogue <sp1441@Sydneys-Air.lan>
2023-07-19 10:11:45 -0400

    added checks for null values for each property of plant before it is used, which will prevent a type error from being thrown if any properties of plant are null. also added a loading state to ensure component doesn't try to access plant properties while data is still being fetched.

--- src/app/individual-plant/page.js
+++ src/app/individual-plant/page.js
@@ -1,9 +1,8 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
 
 export default function IndividualPlant() {
     const [plant, setPlant] = useState(null);
@@ -25,7 +24,10 @@ export default function IndividualPlant() {
 
     }, []);
 
-    console.log('plant me baby', plant);
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
             <Header pageTitle="Plant Name" profileImg="/path_to_profile_image.jpg" />
@@ -34,31 +36,29 @@ export default function IndividualPlant() {
                 <div className="columns is-multiline">
                     <div className="column is-4">
                         <div className="card">
-                            <div className="card">
+                            {plant && plant.image &&
                                 <div className="card-image">
                                     <figure className="image is-4by3">
                                         <img src={plant.image} alt={`Plant ${plant.commonName}`} />
                                     </figure>
                                 </div>
-                            </div>
+                            }
                         </div>
                     </div>
                     <div className="column is-8">
                         <div className="card">
                             <div className="card-content has-text-centered">
-                                <p className="title is-4">{plant.commonName}</p>
+                                {plant.commonName && <p className="title is-4">{plant.commonName}</p>}
                             </div>
                             <br />
                             <div style={{ padding: '20px' }}>
-                                <p className="subtitle is-6">Watering: {plant.wateringDescription}</p>
-                                <p className="subtitle is-6">Sunlight: {plant.sunlightDescription}</p>
-                                <p className="subtitle is-6">Pruning: {plant.pruningDescription}</p>
+                                {plant.wateringDescription && <p className="subtitle is-6">Watering: {plant.wateringDescription}</p>}
+                                {plant.sunlightDescription && <p className="subtitle is-6">Sunlight: {plant.sunlightDescription}</p>}
+                                {plant.pruningDescription && <p className="subtitle is-6">Pruning: {plant.pruningDescription}</p>}
                             </div>
-
                         </div>
                     </div>
                 </div>
-
             </div>
             <br />
             <div className="has-text-centered">
