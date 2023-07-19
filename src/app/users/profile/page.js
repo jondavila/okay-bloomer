e73b0e0bf9b5e96df219d7008b9ff5a2074be5cb
Jonathan Davila <jonathandavila@Jonathans-MacBook-Air.local>
2023-07-18 22:49:27 -0700

    fix: fix localstorage

--- src/app/users/profile/page.js
+++ src/app/users/profile/page.js
@@ -11,15 +11,18 @@ export default function Profile() {
     const [data, setData] = useState(null);
     const [isLoading, setLoading] = useState(true);
 
-    const expirationTime = new Date(parseInt(localStorage.getItem('expiration')) * 1000);
-    let currentTime = Date.now();
-
-    // make a condition that compares exp and current time
-    if (currentTime >= expirationTime) {
-        handleLogout();
-        alert('Session has ended. Please login to continue.');
-        router.push('/users/login');
+    if (typeof window !== 'undefined') {
+        const expirationTime = new Date(parseInt(localStorage.getItem('expiration')) * 1000);
+        let currentTime = Date.now();
+        
+        // make a condition that compares exp and current time
+        if (currentTime >= expirationTime) {
+            handleLogout();
+            alert('Session has ended. Please login to continue.');
+            router.push('/users/login');
+        }
     }
+
 
     useEffect(() => {
         if (localStorage.getItem('jwtToken')) {
