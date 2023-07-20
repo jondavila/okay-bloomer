d837102157e7b0e0e7311951218ed92fa0b9fbf4
JESSICA LEE <jessicalee@MacBook-Pro.askey.com>
2023-07-19 18:32:20 -0700

    styling for login and sign up page

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../header.module.css';
-import './header.css';
 
 const Header = ({ pageTitle, profileImg }) => {
   return (
