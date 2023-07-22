5bb23c4f8e1c23bd2db2642fedd87b8861570f79
Sydney Pogue <sp1441@Sydneys-MacBook-Air.local>
2023-07-22 12:42:40 -0400

    removed journal for now, will be a stretch goal

--- src/app/individual-user-plant/page.js
+++ src/app/individual-user-plant/page.js
@@ -98,16 +98,16 @@ export default function PlantPage() {
                                 </div>
                             </div>
                         </div>
-                        <div className={'card'}>
-                            <div className="card-content has-text-centered">
-                                <p className="subtitle is-6">Add a note about today:</p>
-                                {/* <div className="control">
+                        {/* <div className={'card'}> */}
+                        {/* <div className="card-content has-text-centered"> */}
+                        {/* <p className="subtitle is-6">Add a note about today:</p> */}
+                        {/* <div className="control">
                                     <input className="input" type="text" placeholder="Add Title"></input>
                                     <textarea className="textarea mt-3" placeholder="Journal Entry..."></textarea>
                                 </div> */}
-                                <button className="button is-info is-success">Add to Journal</button>
-                            </div>
-                        </div>
+                        {/* <button className="button is-info is-success">Add to Journal</button> */}
+                        {/* </div> */}
+                        {/* </div> */}
                     </div>
                     <div className="column is-3">
                         <div className={'card'}>
