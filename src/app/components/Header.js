c7843345cc8096e5d1018e2bf3abefefadd4ba9b
Sydney Pogue <sp1441@Sydneys-MacBook-Air.local>
2023-07-22 04:39:13 -0400

    further edited hamburger icon to look better and improve css alignment

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import styles from '../header.module.css';
 import 'animate.css';
@@ -6,18 +6,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Header = ({ pageTitle }) => {
-  const [isActive, setIsActive] = useState(false);
-
-  const handleClick = () => {
-    setIsActive(!isActive);
-  };
 
   return (
     <nav className={`navbar ${styles.myNavbar} animate__animated animate__fadeIn`} role="navigation" aria-label="main navigation" style={{ animationDelay: '0.2s' }}>
       <div className={`${styles.myNavbarItem}`}>
         <Link href="/">
-          <img src="/logo.jpg" alt="Logo" style={{ display: 'inline', width: '30px', height: '30px', marginRight: '10px', fontFamily: 'Preahvihear, sans-serif' }} />
-          <h3 style={{ display: 'inline', color: 'white' }} className="title is-4">Okay Bloomer</h3>
+          <div>
+            <img src="/logo.jpg" alt="Logo" style={{ display: 'inline', width: '30px', height: '30px', marginRight: '10px', fontFamily: 'Preahvihear, sans-serif' }} />
+            <h3 style={{ display: 'inline', color: 'white' }} className="title is-4">Okay Bloomer</h3>
+          </div>
         </Link>
       </div>
 
@@ -25,19 +22,23 @@ const Header = ({ pageTitle }) => {
         <h1 style={{ color: 'white', fontSize: '1.5em' }} className="title is-4">{pageTitle}</h1>
       </div>
 
-      <div className={`${styles.myNavbarItem}`}>
-        <div className={`dropdown ${isActive ? 'is-active' : ''}`}>
+      <div className={`${styles.myNavbarItem} ${styles.dropdownContainer}`}>
+        <div className={`dropdown is-right is-hoverable`}>
           <div className="dropdown-trigger">
-            <button onClick={handleClick} className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <button className={`button ${styles.menuButton}`} aria-haspopup="true" aria-controls="dropdown-menu">
               <span className="icon">
-                <FontAwesomeIcon icon={faBars} /> {/* this is the hamburger icon */}
+                <FontAwesomeIcon icon={faBars} style={{ color: 'white' }} /> {/* this is the hamburger icon */}
               </span>
             </button>
           </div>
           <div className="dropdown-menu" id="dropdown-menu" role="menu">
             <div className="dropdown-content">
-              <Link href="/profile">Profile</Link>
-              <Link href="/sanctuary">Sanctuary</Link>
+              <Link href="/profile">
+                <p className="dropdown-item">Profile</p>
+              </Link>
+              <Link href="/sanctuary">
+                <p className="dropdown-item">Sanctuary</p>
+              </Link>
               {/* Add as many routes as you want */}
             </div>
           </div>
