c9d4d8c16547e7569c2e2a83a5f72cc76ac65f04
Sydney Pogue <sp1441@Sydneys-MacBook-Air.local>
2023-07-22 02:52:13 -0400

    revamped css layout to grid layout for userplants, adjusted appearance animation, and ensured cards would all be the same size

--- src/app/components/UserPlantCardGrid.js
+++ src/app/components/UserPlantCardGrid.js
@@ -1,24 +1,20 @@
+"use client";
+
 import UserPlantCard from './UserPlantCard';
 import 'bulma/css/bulma.min.css';
+import 'animate.css';
 
 
 export default function UserPlantCardGrid({ plantCardsArray, ...props }) {
-    let rows = [];
-    if (plantCardsArray) {
-        plantCardsArray.forEach((plant) => {
-            rows.push(
-                <UserPlantCard
-                    key={plant._id}
-                    plant={plant}
-                />
-            );
-        });
-    }
     return (
         <div className={props.className}>
             <section className="container">
-                <div className="columns features is-multiline">
-                    {rows}
+                <div className="plant-grid">
+                    {plantCardsArray && plantCardsArray.map((plant, index) => (
+                        <div key={plant._id} className={`columns is-variable is-3 animate__animated animate__fadeInUp`} style={{ animationDelay: `${index * 0.2}s` }}>
+                            <UserPlantCard plant={plant} />
+                        </div>
+                    ))}
                 </div>
             </section>
         </div>
